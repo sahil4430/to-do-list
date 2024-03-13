c05989d34011dca7dbaa18ea893d36e923c2c997
Sahil <116063177+sahil4430@users.noreply.github.com>
2024-03-13 21:09:10 +0530

    Update todo.js

---
+++ todo.js
@@ -0,0 +1,7 @@
+let btn=document.querySelector(".submit");
+let input= document.querySelector(".inputbox");
+btn.addEventListener(
+    "click",function(){
+        console.log(input.value)
+    }
+)
