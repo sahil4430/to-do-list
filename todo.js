0d3649a3c3bcbb71e8904e31658eb4135fa51e57
Sahil <116063177+sahil4430@users.noreply.github.com>
2024-03-15 21:26:22 +0530

    Update todo.js
    final js

--- todo.js
+++ todo.js
@@ -1,7 +1,32 @@
-let btn=document.querySelector(".submit");
+let btn= document.querySelector(".submit");
 let input= document.querySelector(".inputbox");
-btn.addEventListener(
-    "click",function(){
-        console.log(input.value)
-    }
-)
+let ul = document.querySelector("ul");
+btn.addEventListener("click",function(){
+  let item =document.createElement("li");
+  item.innerText= input.value;
+  console.log(input.value);
+  let del = document.createElement("img");
+  del.src = "assests/Trash.png";
+  del.classList.add("delete");
+  item.appendChild(del);
+  ul.appendChild(item);
+  input.value = "";
+});
+ul.addEventListener("click",function(event){
+  if(event.target.nodeName.toLowerCase() === "img"){
+      let listItem = event.target.parentElement;
+      listItem.remove();  
+  }
+});
+// let btn=document.querySelector("submit");
+// let input= document.querySelector("inputbox");
+// let ul= document.querySelectorAll("ul")
+// btn.addEventListener(
+//     "click",function(){
+//       let item= document.createElement("li");
+//       item.innerText= input.Value;
+//       console.log(input.Value)
+      
+//     }
+//     // 👨🏼‍🦳👩🏼‍🦳 acha nhi hai complete kro 
+// )
